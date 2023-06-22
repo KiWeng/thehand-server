0cbbd4f750ffc783153cb9cfad7690059c71eee2
kwzh <wzh.k@outlook.com>
2023-06-22 21:35:32 +0800

    Recognition function can properly stop

--- server/app.py
+++ server/app.py
@@ -1,21 +1,27 @@
 import asyncio
 import logging
 import time
+import warnings
 
+import aiohttp
 import numpy as np
 from aiohttp import web
 from mne import filter
 
-# from fake import EegoDriver
+from fake import EegoDriver
 from model.infer import EMGModel
 from utils import DotTimer
-from utils import EegoDriver
+
+# from utils import EegoDriver
 
 log = logging.getLogger(__name__)
 
 board = EegoDriver(sampling_rate=2000)
 model = EMGModel()
 dt = DotTimer()
+
+# supress DeprecationWarning caused by mne filtering function
+warnings.filterwarnings(action='ignore', category=DeprecationWarning)
 
 stds = [
     0.000034,
@@ -86,89 +92,88 @@ R = np.array([[1]])
 kfs = [KalmanFilter(F=F, H=H, Q=Q, R=R, P=P, x=x) for i in range(5)]
 
 
-async def infer(request):
+async def recognition_handler(request):
     model_id = request.match_info['id']
     ws_current = web.WebSocketResponse()
     await ws_current.prepare(request)
     log.info(f"requesting model {model_id}")
-
-    sample_len = 2000
-
-    def step(driver):
-        data = driver.get_data_of_size(sample_len)
-        bipolar_data = data[1]
-        while bipolar_data.shape[0] < sample_len:
-            time.sleep(0.2)
-            data = driver.get_data_of_size(sample_len)
-            bipolar_data = data[1]
-
-        filtered = filter.notch_filter((bipolar_data[:, :12] / stds).T, sample_frequency, freqs,
-                                       filter_length="1000ms", trans_bandwidth=8.0,
-                                       picks=range(12), verbose=False)
-        filt = filter.create_filter(filtered, sample_frequency, l_freq, h_freq, verbose=False)
-        filtered = filter._overlap_add_filter(filtered, filt, picks=[i for i in range(12)]).T
-
-        print(filtered.shape)
-        delay = 100
-        print(filtered[:-delay][-800:, :].std(axis=0))
-
-        # print(normalized_data[-1:, :])
-
-        # normalized_data = np.expand_dims(filtered[:-1000][-800:, :], axis=0)
-        normalized_data = np.expand_dims(filtered[:-delay][-800:, :], axis=0)
-        '''
-        TODO: There are some problems:
-        1. Some times some channels are sinusoidal so it will create large spikes
-         at the start&end of the signal, so head and tail must be cut
-        2. A overlong segment is needed to have a good filtered result
-        '''
-
-        '''
-        20230615
-        The problem still remains, by changing the trans_bandwidth & filter_length the lag have been
-        greatly reduced but still remains (delay can be set to 0 but will the quality of the filtered
-        data will be worsen)
-        Things that affect filtered data quality:
-            change the filtered_slice length (longer is better)
-            change the filter_length&trans_bandwidth in notch_filter parameters
-            padding
-        '''
-
-        prediction = model.predict(normalized_data)  # (samples, channels)
-
-        filtered_pred = []
-        for i in range(len(prediction[0])):
-            kfs[i].predict()
-            kfs[i].update(prediction[0][i])
-            filtered_pred.append(kfs[i].x[0])
-
-        print(prediction)
-        print([filtered_pred])
-
-        return np.asarray([filtered_pred])
-
     await ws_current.send_json({'action': 'connect', 'id': model_id})
-
-    try:
-        while True:
-            # too slow in test
-            # if board.get_board_data_count() < 8 * 800:
-            #     continue
-
-            # elegant, but slower, fuck
-            # prediction = await asyncio.to_thread(infer_step, board)
-            prediction = step(board)
-            await asyncio.sleep(0)
-            await ws_current.send_json(
-                {'action': 'sent', 'prediction': prediction.tolist()}
-            )
-            dt.dot()
-            print(f'{dt.avg}--time elapsed')
-
-    except OSError:
-        log.info(f"stop inferring model {model_id}")
+    await asyncio.gather(close_ws(ws_current), recognition(ws_current, model_id))
 
     return ws_current
+
+
+async def close_ws(ws_current):
+    async for msg in ws_current:
+        if msg.type == aiohttp.WSMsgType.TEXT:
+            if msg.data == 'close':
+                await ws_current.close()
+                await asyncio.sleep(0)
+            else:
+                await ws_current.send_str(msg.data + '/answer')
+        elif msg.type == aiohttp.WSMsgType.ERROR:
+            print('ws connection closed with exception %s' %
+                  ws_current.exception())
+
+
+async def recognition(ws_current, model_id):
+    sample_len = 2000
+
+    try:
+        while not ws_current.closed:
+            data = board.get_data_of_size(sample_len)
+            bipolar_data = data[1]
+            while bipolar_data.shape[0] < sample_len:
+                time.sleep(0.2)
+                data = board.get_data_of_size(sample_len)
+                bipolar_data = data[1]
+
+            filtered = filter.notch_filter((bipolar_data[:, :12] / stds).T, sample_frequency,
+                                           freqs, filter_length="1000ms", trans_bandwidth=8.0,
+                                           picks=range(12), verbose=False)
+            filt = filter.create_filter(filtered, sample_frequency, l_freq, h_freq,
+                                        verbose=False)
+            filtered = filter._overlap_add_filter(filtered, filt,
+                                                  picks=[i for i in range(12)]).T
+
+            delay = 100
+
+            normalized_data = np.expand_dims(filtered[:-delay][-800:, :], axis=0)
+            '''
+            TODO: There are some problems:
+            1. Some times some channels are sinusoidal so it will create large spikes
+             at the start&end of the signal, so head and tail must be cut
+            2. A overlong segment is needed to have a good filtered result
+            '''
+            '''
+            20230615
+            The problem still remains, by changing the trans_bandwidth & filter_length the lag have been
+            greatly reduced but still remains (delay can be set to 0 but will the quality of the
+            filtered data will be worsen)
+            Things that affect filtered data quality:
+                change the filtered_slice length (longer is better)
+                change the filter_length&trans_bandwidth in notch_filter parameters
+                padding
+            '''
+            prediction = model.predict(normalized_data)  # (samples, channels)
+            filtered_pred = []
+            for i in range(len(prediction[0])):
+                kfs[i].predict()
+                kfs[i].update(prediction[0][i])
+                filtered_pred.append(kfs[i].x[0])
+            filtered_pred = np.asarray([filtered_pred])
+
+            '''
+            prediction = await asyncio.to_thread(run_task, params)
+            elegant, but slower, fuck
+            '''
+            await asyncio.sleep(0)
+            await ws_current.send_json(
+                {'action': 'sent', 'prediction': filtered_pred.tolist()}
+            )
+            dt.dot()
+    finally:
+        return
 
 
 async def recording(request):  # TODO
@@ -197,7 +202,7 @@ async def init_app():
     app.on_shutdown.append(shutdown)
 
     app.add_routes([
-        web.get('/infer/{id}', infer),
+        web.get('/infer/{id}', recognition_handler),
         web.get('/', recording)
     ])
 
